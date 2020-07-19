a143f91d4f861353009f608dda19991b46175981
skankylondon <shashank.sings@gmail.com>
2020-07-19 16:58:27 +0100

    first commit

---
+++ Demo/Fizzbuzz.py
@@ -0,0 +1,12 @@
+
+print('enter the range\n')
+marks=int(input())
+for num in range(marks):
+    if(num%3==0 and num%5==0):
+        print('fizzbuzz')
+    elif(num%3==0):
+        print('fizz')
+    elif(num%5==0):
+        print('buzz')
+    else:
+        print(num)
